94ffc0ce47f5b0d17e25cacc5b2d7eb0d0db6ef6
MichelleHirano <michelle@hiranohomes.com>
2021-03-14 15:14:46 -0700

    get items from local storage to appear in reload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
-$(document).ready(function () {
+
 
 //Save Function 
     $(".saveBtn").on("click", function() {
@@ -46,16 +46,12 @@ $(document).ready(function () {
      
 
 //get item from local storage
-$('#hour9.description').val(localStorage.getItem("hour9"));
-$('#hour10.description').val(localStorage.getItem("hour10"));
-$('#hour11.description').val(localStorage.getItem("hour11"));
-$('#hour12.description').val(localStorage.getItem("hour12"));
-$('#hour13.description').val(localStorage.getItem("hour13"));
-$('#hour14.description').val(localStorage.getItem("hour14"));
-$('#hour15.description').val(localStorage.getItem("hour15"));
-$('#hour16.description').val(localStorage.getItem("hour16"));
-$('#hour17.description').val(localStorage.getItem("hour17"));
+window.onload = function() {
+
+    var loadSave = localStorage.getItem(".description");
+    if ("description" !== null) $('#hour9').val("hour9");
+
+}
 
 timeTracker();
 
-})
