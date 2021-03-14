7873df121774f4ef2585f354d2c689b353c9f097
MHirano <michelle@hiranohomes.com>
2021-03-13 17:21:06 -0800

    today's date function

---
+++ script.js
@@ -0,0 +1,13 @@
+//Time and Date displayed at the top of the calendar
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
+
+//Save Function 
+
+//Save text in local storage
+
+//current time
+
+//check time blocks to add background colors
+
+//get item from local storage
