b0babe0fa8a1a87443b84f49c2a99559217238c5
MichelleHirano <michelle@hiranohomes.com>
2021-03-14 18:43:16 -0700

    trying to fix local storage
    items still coming back as undefined

--- script.js
+++ script.js
@@ -2,13 +2,8 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
-var plannerContent= [];
-var getLocalStorageData = JSON.parse(localStorage.getItem("description"));
 
-if(getLocalStorageData !== null){
-    plannerContent = getLocalStorageData;
-}
-
+$(document).ready(function () {
 //Save Function 
     $(".saveBtn").on("click", function() {
         var text = $(this).siblings("description").val();
@@ -50,10 +45,20 @@ if(getLocalStorageData !== null){
     }
  
     
-
-
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
 
 
 timeTracker();
 
+
+})
